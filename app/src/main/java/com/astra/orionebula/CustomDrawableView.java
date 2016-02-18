963f76a84b49979a594a5e07f9484429164aa382
seby85ct <seby85ct@hotmail.com>
2016-02-18 22:10:37 +0100

    L'esagono è stampato su schermo

--- app/src/main/java/com/astra/orionebula/CustomDrawableView.java
+++ app/src/main/java/com/astra/orionebula/CustomDrawableView.java
@@ -2,6 +2,7 @@ package com.astra.orionebula;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.view.View;
@@ -10,7 +11,7 @@ import android.view.View;
  * Created by Seby on 12/02/2016.
  */
 public class CustomDrawableView extends View {
-    private ShapeDrawable mDrawable;
+    private HexagonDrawable mDrawable;
 
     public CustomDrawableView(Context context) {
         super(context);
@@ -20,9 +21,8 @@ public class CustomDrawableView extends View {
         int width = 300;
         int height = 50;
 
-        mDrawable = new ShapeDrawable(new OvalShape());
-        mDrawable.getPaint().setColor(0xff74AC23);
-        mDrawable.setBounds(x, y, x + width, y + height);
+        mDrawable = new HexagonDrawable(0xff74AC23);
+        mDrawable.computeHex(new Rect(100,200, 500, 500));
     }
 
     protected void onDraw(Canvas canvas) {
--- app/src/main/java/com/astra/orionebula/MainActivity.java
+++ app/src/main/java/com/astra/orionebula/MainActivity.java
@@ -1,17 +1,56 @@
 package com.astra.orionebula;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends Activity {
 
     CustomDrawableView mCustomDrawableView;
 
-    @Override
+//    LinearLayout mLinearLayout;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a LinearLayout in which to add the ImageView
+//        mLinearLayout = new LinearLayout(this);
+
+
+//        HexagonDrawable myImage = new HexagonDrawable(60000);
+//        myImage.computeHex(new Rect(10, 10, 30, 40));
+        // Instantiate an ImageView and define its properties
+//        ImageView i = new ImageView(this);
+        //i.setImageDrawable(myImage);
+//        i.setAdjustViewBounds(true); // set the ImageView bounds to match the Drawable's dimensions
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        i.setLayoutParams(layoutParams);
+
+        // Add the ImageView to the layout and set the layout as the content view
+ //       mLinearLayout.addView(i);
+        mCustomDrawableView = new CustomDrawableView(this);
+
+        setContentView(mCustomDrawableView);
+
+    }
+
+
+
+   /* @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
@@ -20,7 +59,7 @@ public class MainActivity extends Activity {
 
         setContentView(mCustomDrawableView);
     }
-
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
